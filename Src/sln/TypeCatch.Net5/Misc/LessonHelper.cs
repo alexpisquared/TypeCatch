3ce2e392d9773bb4bf966b27cd7a34ea4d69eb2f
AlexPi <alex.pigida@outlook.com>
2021-12-05 17:28:58 -0500

    Update LessonHelper.cs

--- Src/sln/TypeCatch.Net5/Misc/LessonHelper.cs
+++ Src/sln/TypeCatch.Net5/Misc/LessonHelper.cs
@@ -140,33 +140,33 @@ namespace TypingWpf
     public static string ExercizeDir => _exercizeDir;
     #region Excercises - hardcoded and defaults:
     const string
-            _ght = @"ght ght ght ght ght ght ght ght ght ght 
-night night night night night night night night night night 
-might might might might might might might might might might 
+            _ght = @"ght ght ght ght ght ght ght ght ght ght
+night night night night night night night night night night
+might might might might might might might might might might
 midnight midnight midnight midnight midnight midnight midnight midnight midnight ",
             _msa = @"{0} {0} {0} {0} {0} {0} {0} {0} {0} {0} {1} {1} {1} {1} {1} {1} {1} {0}.{1} {0}.{1} {0}.{1} {0}.{1} @outlook.com @outlook.com @outlook.com @outlook.com {0}.{1}@outlook.com {0}.{1}@outlook.com {0}.{1}@outlook.com {0}.{1}@outlook.com",
-            _nym = @"nymi nymi nymi nymi nymi nymi nymi nymi nymi nymi 
-@nymi.com @nymi.com @nymi.com @nymi.com @nymi.com 
+            _nym = @"nymi nymi nymi nymi nymi nymi nymi nymi nymi nymi
+@nymi.com @nymi.com @nymi.com @nymi.com @nymi.com
 a{0}@nymi.com a{0}@nymi.com a{0}@nymi.com ",
-            _the = @"the the the the the the the the the the 
-them them them them them them them them them them 
-then then then then then then then then then then 
+            _the = @"the the the the the the the the the the
+them them them them them them them them them them
+then then then then then then then then then then
 there there there there there there there there there there ",
-            _was = @"was was was was was was was was was was 
-wase wase wase wase wase wase wase wase wase wase 
-wased wased wased wased wased wased wased wased wased wased 
-wax wax wax wax wax wax wax wax wax wax 
-waxe waxe waxe waxe waxe waxe waxe waxe waxe waxe 
+            _was = @"was was was was was was was was was was
+wase wase wase wase wase wase wase wase wase wase
+wased wased wased wased wased wased wased wased wased wased
+wax wax wax wax wax wax wax wax wax wax
+waxe waxe waxe waxe waxe waxe waxe waxe waxe waxe
 waxed waxed waxed waxed waxed waxed waxed waxed waxed waxed ",
-            _whe = @"what what what what what what what what what what 
-when when when when when when when when when when 
-where where where where where where where where where where 
-why why why why why why why why why why 
+            _whe = @"what what what what what what what what what what
+when when when when when when when when when when
+where where where where where where where where where where
+why why why why why why why why why why
 was was was was was was was was was was ",
-            _tion = @"tion tion tion tion tion tion tion tion tion tion 
-fiction fiction fiction fiction fiction fiction fiction fiction fiction fiction 
-mention mention mention mention mention mention mention mention mention mention 
-tention tention tention tention tention tention tention tention tention tention 
+            _tion = @"tion tion tion tion tion tion tion tion tion tion
+fiction fiction fiction fiction fiction fiction fiction fiction fiction fiction
+mention mention mention mention mention mention mention mention mention mention
+tention tention tention tention tention tention tention tention tention tention
 sanction sanction sanction sanction sanction sanction sanction sanction sanction sanction ",
             _exercizeDir = @"Public\AppData\TypeCatch\Excersize\",
           _basicLessons = @"[#] df  jk
@@ -295,17 +295,6 @@ $$$((( $$($(($$(( $($($ (($($ $84 $0 $($$(($($( $74 $2.94 $8.49 £££((( ££(�
 7/8/9¶5/6/4¶3/1/2¶9/5/1¶87/46/32¶8*7*9¶5*6*4¶2*1*3¶7*5*3¶89*46*12¶7-9-8¶5-6-4¶3-1-2¶8-4-3¶79-65-20¶9+7+8¶5+4+6¶2+1+3¶7+6+2¶98+54+30¶7.98¶5.64¶3.12¶0.951¶3.14¶7/5*3-1+3.08¶76+84-943+80+215-30-51¶286/14*9*372/84*50/306¶.315*.486+1.509-.75/6.02¶45+5.18+862-.75-13/.94/26*3.14*80+1.27*50-6.52/9
 
 ";
-
-    public enum LessonType
-    {
-      BasicLessons,
-      Combinations,
-      DigitSymbols,
-      SpecialDrill,
-      PhrasesRandm,
-      Experimental,
-      EditableFile
-    }
     #endregion
 
     #region Core texts:
@@ -373,7 +362,7 @@ Too bad that all the people who know how to run the country are busy driving tax
 You need to be comfortable with you. Stop worrying about the couple in the corner who may or may not be looking at you funny.
 Tension is a habit. Relaxing is a habit. Bad habits can be broken, good habits formed.
 How can they say my life is not a success? Have I not for more than sixty years gotten enough to eat and escaped being eaten?
-Chasing success is like trying to squeeze a handful of water. The tighter you squeeze, the less water you get. 
+Chasing success is like trying to squeeze a handful of water. The tighter you squeeze, the less water you get.
 When you chase it, your life becomes the chase, and you become a victim of always wanting more.
 Going to church doesn't make you a Christian any more than going to a garage makes you an automobile.
 Isn't it interesting that the same people who laugh at science fiction listen to weather forecasts and economists?
@@ -396,7 +385,7 @@ Nothing in the world is ever completely wrong. Even a stopped clock is right twi
 Wrinkles should merely indicate where smiles have been.
 Be nice to nerds. Chances are you'll end up working for one.
 You can tell a lot about a person by the way they handle three things: a rainy day, lost luggage and tangled Christmas tree lights.
-I ask people if an elephant has ever bitten them. Most of the time people say no. But everyone has been bitten by a mosquito. 
+I ask people if an elephant has ever bitten them. Most of the time people say no. But everyone has been bitten by a mosquito.
 It's the little things that get us.
 Money is like manure, of very little use except it be spread.
 Enthusiasm is the leaping lightning, not to be measured by the horse-power of the understanding.
@@ -405,7 +394,7 @@ An appeaser is one who feeds a crocodile, hoping it will eat him last.
 Aerodynamically the bumblebee shouldn't be able to fly, but the bumblebee doesn't know that so it goes on flying anyway.
 No one has ever become poor by giving.
 I am not funny. My writers were funny. My direction was funny. The situations were funny. But I am not funny. I am not funny. What I am is brave.
-Do not hold to what you have. It is like a ferry boat for people who want to get across waters. 
+Do not hold to what you have. It is like a ferry boat for people who want to get across waters.
 Once you have got across, never bear it on your back. You should head forward.
 Life is the movie you see through your own eyes. It makes little difference what's happening out there. It's how you take it that counts.
 Here is the test to find whether your mission on earth is finished. If you're alive, it isn't.
@@ -417,8 +406,8 @@ To die laughing must be the most glorious of all glorious deaths!
 He knows nothing; he thinks he knows everything - that clearly points to a political career.
 If a cluttered desk is a sign of a cluttered mind, of what, then, is an empty desk a sign?
 Did you ever notice how difficult it is to argue with someone who is not obsessed with being right?
-Years ago there was belief that the world was flat. 
-People were born into that belief and they took it on faith that if they went too far from the shoreline in a boat they would fall off the earth. 
+Years ago there was belief that the world was flat.
+People were born into that belief and they took it on faith that if they went too far from the shoreline in a boat they would fall off the earth.
 Columbus sailed on.
 Happen to things, don't let things happen to you.
 Life is a hurdle race, the winner has to cross all the hurdles and still maintain enthusiasm.
@@ -439,13 +428,13 @@ Quality is more than important than quantity. One home run is much better than t
 Why hoard your troubles? They have no market value, so just throw them away.
 Procrastination is an art form that is less desirable than painting a detailed landscape using a three inch wide brush.
 Without a plan of action to put how a dream is envisioned to play out is like pouring out of picture of water on the ground and expecting it to stay in on the surface of the ground.
-Negative people aren't concentrating their bad energy to be shared with only you. 
+Negative people aren't concentrating their bad energy to be shared with only you.
 Mosquitos aren't going to bite just you; they are going to go after the easiest available blood.
 Don't sweat the petty things and don't pet the sweaty things.
 The really frightening thing about middle age is the knowledge that you'll grow out of it.
 I was thinking about how people seem to read the Bible a whole lot more as they get older; then it dawned on me - they're cramming for their final exam.
 Turning our abilities from stagnate puddles to rushing rivers can happen when we apply ourselves to the task.
-Our minds are like monkeys, swinging from one thought to another like monkeys on a tree. 
+Our minds are like monkeys, swinging from one thought to another like monkeys on a tree.
 As a result, we always feel as if a sense of order, balance, awareness and concentration elude us, because we are always doing, always acting.
 An economist is an expert who will know tomorrow why the things he predicted yesterday didn't happen today.
 Remember that sometimes people laugh when something is actually funny, but often they laugh when they lack the imagination to understand the situation.
@@ -459,8 +448,8 @@ The trouble with not having a goal is that you can spend your life running up an
 If you had to identify, in one word, the reason why the human race has not achieved, and never will achieve, its full potential, that word would be meetings.
 A funny thing happens when we start keeping promises to ourselves - we become unstoppable.
 Visualize something totally funny or crazy! This will instantly change how you feel because you can't visualize two things at the same time.
-I am not an advocate of pity parties. They allow you to wallow in misery and drag yourself even further into the depths of woe. 
-But sometimes having a good cry is like changing the oil in your car. 
+I am not an advocate of pity parties. They allow you to wallow in misery and drag yourself even further into the depths of woe.
+But sometimes having a good cry is like changing the oil in your car.
 You got 3000 miles out of that batch, now you have 3000 miles to go before another.
 To be taught to read - what is the use of that, if you know not whether what you read is false or true?
 If everything is coming your way, then you're in the wrong lane.
@@ -1627,5 +1616,16 @@ Andy looks at it a long time.
 And then he unfolds to me an instantaneous idea that has occurred to him.
 Right there was organized a trust; and we walked back into town and put it on the market.";
     #endregion
+  }
+
+  public enum LessonType
+  {
+    BasicLessons,
+    Combinations,
+    DigitSymbols,
+    SpecialDrill,
+    PhrasesRandm,
+    Experimental,
+    EditableFile
   }
 }
