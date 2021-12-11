1aa122ca853aa66e4363e30391d248483e3e4f9b
AlexPi <alex.pigida@outlook.com>
2021-12-11 16:11:31 -0500

    current speed smoothered by animation!!!!!!!

--- Src/sln/TypeCatch.Net5/Vws/MainWindow.xaml.cs
+++ Src/sln/TypeCatch.Net5/Vws/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using AAV.Sys.Helpers;
 using AsLink;
+using System.Diagnostics;
 using System.Windows;
 
 namespace TypingWpf
@@ -13,10 +14,17 @@ namespace TypingWpf
       _ignoreEscape = true;
 
       VersioInfo.Text = $"{VerHelper.CurVerStr(".Net5")}\n{DbMdl.A0DbMdl.GetA0DbMdlAzureDb.ServerDatabase()}";
+
+      Topmost = Debugger.IsAttached;
     }
 
     void onWindowMinimize(object s, RoutedEventArgs e) => WindowState = System.Windows.WindowState.Minimized;
     void onWindowRestoree(object s, RoutedEventArgs e) { wr.Visibility = Visibility.Collapsed; wm.Visibility = Visibility.Visible; WindowState = System.Windows.WindowState.Normal; }
     void onWindowMaximize(object s, RoutedEventArgs e) { wm.Visibility = Visibility.Collapsed; wr.Visibility = Visibility.Visible; WindowState = System.Windows.WindowState.Maximized; }
+
+    private void ProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+    {
+
+    }
   }
 }
