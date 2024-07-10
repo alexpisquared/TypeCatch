8508c4a1fcc80859c3abaab63446d0a33184e2cd
alexpisqueared <alex.pigida@outlook.com>
2024-07-10 17:34:55 -0400

    pau

--- Src/sln/TypeCatch.Net5/Misc/CartesianChartExt.cs
+++ Src/sln/TypeCatch.Net5/Misc/CartesianChartExt.cs
@@ -1,43 +1,24 @@
-﻿//using AAV.Sys.Ext;
-using AsLink;
+﻿using System.Windows.Media;
 using LiveCharts;
 using LiveCharts.Configurations;
-using LiveCharts.Wpf;
-using System;
-using System.Collections.Generic;
-using System.Windows.Media;
-using db = TypingWpf.DbMdl;
 
-namespace TypingWpf
+namespace TypeCatch.Net5.Misc;
+
+public static class CartesianChartExt
 {
-    public static class CartesianChartExt
+  public static void LoadDataToChart(this CartesianChart cc, IEnumerable<SessionResult> sr)
+  {
+    try
     {
-        public static void LoadDataToChart(this CartesianChart cc, IEnumerable<db.SessionResult> sr)
-        {
-            try
-            {
-                var dayConfig = Mappers.Xy<DateMdl>().X(datemdl => datemdl.DTime.ToOADate()).Y(datemdl => datemdl.Value); //you can also configure this type globally, so you don't need to configure every SeriesCollection instance using the type. more info at http://lvcharts.net/App/Index#/examples/v1/wpf/Types%20and%20Configuration
+      var dayConfig = Mappers.Xy<DateMdl>().X(datemdl => datemdl.DTime.ToOADate()).Y(datemdl => datemdl.Value); //you can also configure this type globally, so you don't need to configure every SeriesCollection instance using the type. more info at http://lvcharts.net/App/Index#/examples/v1/wpf/Types%20and%20Configuration
 
-                cc.Series = new SeriesCollection(dayConfig) {
-                    new LineSeries { Values = new ChartValues<DateMdl>(),
-                        PointGeometry = DefaultGeometries.Cross,
-                        PointGeometrySize = 6,
-                        PointForeground = Brushes.Yellow,
-                        LineSmoothness = .01
-                    }
-                };
+      cc.Series = new SeriesCollection(dayConfig) { new LineSeries { Values = new ChartValues<DateMdl>(), PointGeometry = DefaultGeometries.Circle, PointGeometrySize = 4, PointForeground = Brushes.Yellow, LineSmoothness = .01 } };
 
-                foreach (var l2 in sr)
-                    cc.Series[0].Values.Add(new DateMdl { DTime = l2.DoneAt, Value = l2.CpM });
-
-            }
-            catch (Exception ex) { ex.Log(); }
-        }
+      foreach (var l2 in sr)
+        _ = cc.Series[0].Values.Add(new DateMdl(l2.DoneAt, l2.CpM));
     }
-
-    public class DateMdl
-    {
-        public DateTime DTime { get; set; }
-        public double Value { get; set; }
-    }
+    catch (Exception ex) { _ = ex.Log(); }
+  }
 }
+
+public record DateMdl(DateTime DTime, double Value);
